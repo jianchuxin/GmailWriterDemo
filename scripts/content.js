f4b9cd0ed478364c051705a972fe1013d1921b6c
jianchuxin <jianchuxin@qq.com>
2023-03-31 17:14:06 +0800

    didnot work

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,13 @@
 
-const form = document.querySelector(':eh');
+const form = document.querySelector('form');
+console.log(form);
+// const writeBox = document.createElement('div');
+// form.insertAdjacentElement('beforeend',writeBox);
 
-const writeBox = document.createElement('div');
-const img = document.createElement()
+
+// const logoButton = document.createElement('div');
+// logoButton.insertAdjacentHTML(`<img src = ${chrome.runtime.getURL('images/icon-16.png')}>`);
+
+// writeBox.insertAdjacentElement('beforebegin',logoButton);
+
 
