3e8d766ee4f1333899adffe622d15c00888e1519
jianchuxin <jianchuxin@qq.com>
2023-04-04 18:11:39 +0800

    continue working

---
+++ background.js
@@ -0,0 +1,25 @@
+// background.js
+
+// Wrap in an onInstalled callback in order to avoid unnecessary work
+// every time the background script is run
+chrome.runtime.onInstalled.addListener(() => {
+    // Page actions are disabled by default and enabled on select tabs
+    chrome.action.disable();
+  
+    // Clear all rules to ensure only our expected rules are set
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+      // Declare a rule to enable the action on example.com pages
+      let exampleRule = {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostSuffix: '.mail.google.com',schemes:['https']},
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowAction()],
+      };
+  
+      // Finally, apply our new array of rules
+      let rules = [exampleRule];
+      chrome.declarativeContent.onPageChanged.addRules(rules);
+    });
+  });
